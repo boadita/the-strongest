f8107b6d05604a92e6b9299de7ec65cff4e389fe
boadita <sboadam@gmail.com>
2026-02-18 14:32:16 -0400

    asides modified

--- src/features/estadisticas/Internacionales.jsx
+++ src/features/estadisticas/Internacionales.jsx
@@ -103,7 +103,7 @@ const Internacionales = () => {
                     </ul>
                 </aside>
                 <section>
-                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         {partidosPorAnio.map((partido) => (
                             <div
                                 key={partido.Id}
