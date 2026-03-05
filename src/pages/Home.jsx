c8754d8d0a5dd8feebeac1d6b54089d8d0949ebc
boadita <sboadam@gmail.com>
2026-03-05 01:15:13 -0400

    nuevas RESULTADOS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,31 +11,31 @@ const Home = () => {
           <h3 className="font-bold text-xl mb-1 text-center">ÚLTIMO PARTIDO</h3>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">FECHA: </h4>
-            <span className="text-2x1">1-3-2026</span>
+            <span className="text-2x1">4-3-2026</span>
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">CIUDAD: </h4>
-            <span className="text-2x1">POTOSÍ</span>
+            <span className="text-2x1">LA PAZ</span>
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">ESTADIO: </h4>
-            <span className="text-2x1">VÍCTOR AGUSTÍN UGARTE</span>
+            <span className="text-2x1">HERNANDO SILES</span>
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">TORNEO: </h4>
             <span className="text-2x1">REPECHAJE VERANO</span>
           </div>
           <div className="flex flex-col items-center justify-center p-4 gap-2 border border-black rounded-lg">
-            <div className="font-bold text-lg">NACIONAL POTOSÍ</div>
-            <div className="text-2x1 text-black">2-0</div>
             <div className="font-bold text-lg">THE STRONGEST</div>
+            <div className="text-2x1 text-black">5-1</div>
+            <div className="font-bold text-lg">NACIONAL POTOSÍ</div>
           </div>
         </div>
         <div className="bg-black text-white p-4 rounded-xl shadow lg:w-1/2">
           <h3 className="font-bold text-xl mb-1 text-center">PRÓXIMO PARTIDO</h3>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">FECHA: </h4>
-            <span className="text-2x1">4-3-2026</span>
+            <span className="text-2x1">7-3-2026</span>
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">CIUDAD: </h4>
@@ -52,7 +52,7 @@ const Home = () => {
           <div className="flex flex-col items-center justify-center p-4 gap-2 border border-white rounded-lg">
             <div className="font-bold text-lg">THE STRONGEST</div>
             <div className="text-2x1 text-white">vs</div>
-            <div className="font-bold text-lg">NACIONAL POTOSÍ</div>
+            <div className="font-bold text-lg">ORIENTE PETROLERO</div>
           </div>
         </div>
       </div>
