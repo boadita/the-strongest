ebc4161282d04be2d107ec3e6510f36a7e78449e
boadita <sboadam@gmail.com>
2026-05-10 23:15:19 -0400

    NUEVO MAYO

--- src/pages/Fixture.jsx
+++ src/pages/Fixture.jsx
@@ -9,7 +9,7 @@ import bolivar from "../assets/rivales/bolivar.png";
 const Fixture = () => {
     return (
         <div className="space-y-6">
-            <h1 className="font-bold dark:text-gray-800 text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl">PARTIDOS ABRIL</h1>
+            <h1 className="font-bold dark:text-gray-800 text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl">PARTIDOS MAYO</h1>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-10">
                 <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
                     <img
