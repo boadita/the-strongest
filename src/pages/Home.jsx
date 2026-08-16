319e5d100bd995145e42f1d06ae0e238325ac9ea
boadita <sboadam@gmail.com>
2026-08-15 22:53:57 -0400

    15 de agosto de 2026

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,31 @@ const Home = () => {
           <h3 className="font-bold text-xl mb-1 text-center">ÚLTIMO PARTIDO</h3>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">FECHA: </h4>
-            <span className="text-2x1">8-8-2026</span>
+            <span className="text-2x1">15-8-2026</span>
+          </div>
+          <div className="flex items-center gap-2 p-1">
+            <h4 className="font-bold text-2x1">CIUDAD: </h4>
+            <span className="text-2x1">LA PAZ</span>
+          </div>
+          <div className="flex items-center gap-2 p-1">
+            <h4 className="font-bold text-2x1">ESTADIO: </h4>
+            <span className="text-2x1">GILBERTO PARADA</span>
+          </div>
+          <div className="flex items-center gap-2 p-1">
+            <h4 className="font-bold text-2x1">TORNEO: </h4>
+            <span className="text-2x1">LIGA FBF</span>
+          </div>
+          <div className="flex flex-col items-center justify-center p-4 gap-2 border border-black rounded-lg">
+            <div className="font-bold text-lg">GUABIRÁ</div>
+            <div className="text-2x1 text-black">3-1</div>
+            <div className="font-bold text-lg">THE STRONGEST</div>
+          </div>
+        </div>
+        <div className="bg-black text-white p-4 rounded-xl shadow lg:w-1/2">
+          <h3 className="font-bold text-xl mb-1 text-center">PRÓXIMO PARTIDO</h3>
+          <div className="flex items-center gap-2 p-1">
+            <h4 className="font-bold text-2x1">FECHA: </h4>
+            <span className="text-2x1">18-8-2026</span>
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">CIUDAD: </h4>
@@ -23,36 +47,12 @@ const Home = () => {
           </div>
           <div className="flex items-center gap-2 p-1">
             <h4 className="font-bold text-2x1">TORNEO: </h4>
-            <span className="text-2x1">LIGA FBF</span>
-          </div>
-          <div className="flex flex-col items-center justify-center p-4 gap-2 border border-black rounded-lg">
-            <div className="font-bold text-lg">THE STRONGEST</div>
-            <div className="text-2x1 text-black">1-2</div>
-            <div className="font-bold text-lg">INDEPENDIENTE PETROLERO</div>
-          </div>
-        </div>
-        <div className="bg-black text-white p-4 rounded-xl shadow lg:w-1/2">
-          <h3 className="font-bold text-xl mb-1 text-center">PRÓXIMO PARTIDO</h3>
-          <div className="flex items-center gap-2 p-1">
-            <h4 className="font-bold text-2x1">FECHA: </h4>
-            <span className="text-2x1">15-8-2026</span>
-          </div>
-          <div className="flex items-center gap-2 p-1">
-            <h4 className="font-bold text-2x1">CIUDAD: </h4>
-            <span className="text-2x1">MONTERO</span>
-          </div>
-          <div className="flex items-center gap-2 p-1">
-            <h4 className="font-bold text-2x1">ESTADIO: </h4>
-            <span className="text-2x1">GILBERTO PARADA</span>
-          </div>
-          <div className="flex items-center gap-2 p-1">
-            <h4 className="font-bold text-2x1">TORNEO: </h4>
-            <span className="text-2x1">LIGA FBF</span>
+            <span className="text-2x1">COPA PACEÑA</span>
           </div>
           <div className="flex flex-col items-center justify-center p-4 gap-2 border border-white rounded-lg">
-            <div className="font-bold text-lg">GUABIRÁ</div>
-            <div className="text-2x1 text-white">vs</div>
             <div className="font-bold text-lg">THE STRONGEST</div>
+            <div className="text-2x1 text-white">vs</div>
+            <div className="font-bold text-lg">ORIENTE PETROLERO</div>
           </div>
         </div>
       </div>
