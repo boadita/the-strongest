1fec971f0e5e1d2743fdf69539f366df943f7fca
boadita <sboadam@gmail.com>
2026-09-03 23:38:40 -0400

    PARTIDOS SEPTIEMBRE

--- src/pages/Fixture.jsx
+++ src/pages/Fixture.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import nacional_potosi from "../assets/rivales/nacional_potosi.png";
-import inde_petro from "../assets/rivales/inde-petro.png";
+import real_tomayapo from "../assets/rivales/real_tomayapo.png";
+import real_oruro from "../assets/rivales/real-oruro.png";
 import guabira from "../assets/rivales/guabira.png";
-import oriente_petrolero from "../assets/rivales/oriente-petrolero.png";
+import ABB from "../assets/rivales/ABB.png";
 import fc_universitario from "../assets/rivales/fc_universitario.png";
-import san_antonio from "../assets/rivales/san_antonio.png";
+import oriente_petrolero from "../assets/rivales/oriente-petrolero.png";
 import bolivar from "../assets/rivales/bolivar.png";
 
 const Fixture = () => {
@@ -14,38 +14,11 @@ const Fixture = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 p-10">
                 <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
                     <img
-                        src={nacional_potosi}
-                        alt="nacional_potosi"
+                        src={real_tomayapo}
+                        alt="real_tomayapo"
                         className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
                     />
-                    <h2 className='font-bold text-center mt-2'>5-8-2026</h2>
-                    <p className='font-bold text-center mt-2'>ESTADIO VÍCTOR AGUSTÍN UGARTE</p>
-                </div>
-                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
-                    <img
-                        src={inde_petro}
-                        alt="inde_petro"
-                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
-                    />
-                    <h2 className='font-bold text-center mt-2'>8-8-2026</h2>
-                    <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
-                </div>
-                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
-                    <img
-                        src={guabira}
-                        alt="guabira"
-                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
-                    />
-                    <h2 className='font-bold text-center mt-2'>15-8-2026</h2>
-                    <p className='font-bold text-center mt-2'>ESTADIO GILBERTO PARADA</p>
-                </div>
-                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
-                    <img
-                        src={oriente_petrolero}
-                        alt="oriente_petrolero"
-                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
-                    />
-                    <h2 className='font-bold text-center mt-2'>18-8-2026</h2>
+                    <h2 className='font-bold text-center mt-2'>6-9-2026</h2>
                     <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
                 </div>
                 <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
@@ -54,17 +27,26 @@ const Fixture = () => {
                         alt="fc_universitario"
                         className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
                     />
-                    <h2 className='font-bold text-center mt-2'>21-8-2026</h2>
+                    <h2 className='font-bold text-center mt-2'>9-9-2026</h2>
+                    <p className='font-bold text-center mt-2'>ESTADIO FÉLIX CAPRILES</p>
+                </div>
+                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
+                    <img
+                        src={real_oruro}
+                        alt="real_oruro"
+                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
+                    />
+                    <h2 className='font-bold text-center mt-2'>12-9-2026</h2>
                     <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
                 </div>
                 <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
                     <img
-                        src={san_antonio}
-                        alt="san_antonio"
+                        src={ABB}
+                        alt="ABB"
                         className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
                     />
-                    <h2 className='font-bold text-center mt-2'>26-8-2026</h2>
-                    <p className='font-bold text-center mt-2'>ESTADIO FÉLIX CAPRILES</p>
+                    <h2 className='font-bold text-center mt-2'>15-9-2026</h2>
+                    <p className='font-bold text-center mt-2'>ESTADIO VILLA INGENIO</p>
                 </div>
                 <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
                     <img
@@ -72,7 +54,25 @@ const Fixture = () => {
                         alt="bolivar"
                         className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
                     />
-                    <h2 className='font-bold text-center mt-2'>30-8-2026</h2>
+                    <h2 className='font-bold text-center mt-2'>20-9-2026</h2>
+                    <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
+                </div>
+                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
+                    <img
+                        src={guabira}
+                        alt="guabira"
+                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
+                    />
+                    <h2 className='font-bold text-center mt-2'>26-9-2026</h2>
+                    <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
+                </div>
+                <div className="bg-yellow-100 dark:text-gray-800 p-4 rounded-xl shadow">
+                    <img
+                        src={oriente_petrolero}
+                        alt="oriente_petrolero"
+                        className="mx-auto w-40 h-40 md:w-48 md:h-48 object-contain aspect-square"
+                    />
+                    <h2 className='font-bold text-center mt-2'>30-9-2026</h2>
                     <p className='font-bold text-center mt-2'>ESTADIO HERNANDO SILES</p>
                 </div>
             </div>
